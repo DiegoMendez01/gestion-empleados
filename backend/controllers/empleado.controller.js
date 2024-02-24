137daf92d1b1260ac30ea3acf45c68a5626d3b56
DESKTOP-GJ24RS2\Diego <diegomendez01soft@gmail.com>
2024-02-24 18:35:59 -0500

    Implementar metodo para obtener un empleado, actualizar o eliminar empleado

--- backend/controllers/empleado.controller.js
+++ backend/controllers/empleado.controller.js
@@ -21,3 +21,25 @@ empleadoCtrl.getEmpleado = async (req, res) => {
     const empleado = await Empleado.findById(req.params.id);
     res.json(empleado);
 }
+
+//Actualizar empleado
+empleadoCtrl.editEmpleado = async (req, res) => {
+    const {id} = req.params;
+    const empleadoEdit = {
+        name: req.body.name,
+        position: req.body.position,
+        office: req.body.office,
+        salary: req.body.salary
+    }
+    await Empleado.findByIdAndUpdate(id, {$set: empleadoEdit}, {new: true});
+    res.json({status: 'Empleado actualizado'});
+}
+
+//Eliminar empleado
+empleadoCtrl.deleteEmpleado = async (req, res) => {
+    await Empleado.findByIdAndDelete(req.params.id);
+    res.json({status: 'Empleado Eliminado'});
+}
+
+//Expotar modulo
+module.exports = empleadoCtrl;
