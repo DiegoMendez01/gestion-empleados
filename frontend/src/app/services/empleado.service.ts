e9f453eca71487f39eefba03feec0b185c29489f
DESKTOP-GJ24RS2\Diego <diegomendez01soft@gmail.com>
2024-02-24 20:15:36 -0500

    Implementar los servicios del empleado para la URL API

--- frontend/src/app/services/empleado.service.ts
+++ frontend/src/app/services/empleado.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Empleados } from '../models/empleados';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmpleadoService {
 
-  constructor() { }
+  selectedEmpleado: Empleados;
+  empleados: Empleados[];
+  readonly URL_API = 'http://localhost:3000/api/empleados'
+
+  constructor(private http: HttpClient){
+    this.selectedEmpleado = new Empleados();
+    this.empleados        = [];
+  }
+
+  getEmpleados(){
+    return this.http.get(this.URL_API);
+  }
+
+  postEmpleado(Empleados : Empleados){
+    return this.http.post(this.URL_API, Empleados);
+  }
+
+  putEmpleado(Empleados : Empleados){
+    return this.http.post(this.URL_API + `/${Empleados._id}`, Empleados);
+  }
+
+  deleteEmpleado(_id : String){
+    return this.http.delete(this.URL_API + `/${_id}`);
+  }
 }
