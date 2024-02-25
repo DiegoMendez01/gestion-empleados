3a35c50995eaf0e4d7a18975fcf95b6ecb65de16
DESKTOP-GJ24RS2\Diego <diegomendez01soft@gmail.com>
2024-02-24 20:04:20 -0500

    Modelo de empleado

--- frontend/src/app/models/empleados.ts
+++ frontend/src/app/models/empleados.ts
@@ -1,2 +1,16 @@
 export class Empleados {
+    constructor(_id = "", name = "", position = "", office = "", salary = 0){
+        this._id = _id,
+        this.name = name,
+        this.position = position,
+        this.office = office,
+        this.salary = salary
+    }
+
+    // Sub guion id porque los datos van a venir de MongoDB
+    _id: String;
+    name: String;
+    position: String;
+    office: String;
+    salary: number;
 }
